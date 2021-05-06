b5dc0fcbe66b8ff2d2de9fb64f777ec82bc70394
Dan Siegel <me@dansiegel.net>
2021-05-06 15:52:20 -0700

    template update

--- sample/PrismMauiDemo/MainWindow.cs
+++ sample/PrismMauiDemo/MainWindow.cs
@@ -1,13 +1,16 @@
 ﻿using Microsoft.Maui;
-using Microsoft.Maui.Controls;
 
 namespace PrismMauiDemo
 {
-	public class MainWindow : Window
-	{
-		public MainWindow()
-			: base(new MainPage())
-		{
-		}
-	}
+    public class MainWindow : IWindow
+    {
+        public MainWindow()
+        {
+            Page = new MainPage();
+        }
+
+        public IPage Page { get; set; }
+
+        public IMauiContext MauiContext { get; set; }
+    }
 }
