9ad002443c8df7ef062fb90e45671d3e31fbfe2b
Dan Siegel <me@dansiegel.net>
2021-09-17 07:24:11 -0700

    ensure module services only get registered once

--- src/Prism.Maui/PrismAppBuilderExtensions.cs
+++ src/Prism.Maui/PrismAppBuilderExtensions.cs
@@ -7,6 +7,8 @@ namespace Prism
 {
     public static class PrismAppBuilderExtensions
     {
+        private static bool s_didRegisterModules = false;
+
         public static PrismAppBuilder OnInitialized(this PrismAppBuilder builder, Action action)
         {
             return builder.OnInitialized(_ => action());
@@ -18,11 +20,16 @@ namespace Prism
         /// <param name="moduleCatalog">The ModuleCatalog to configure</param>
         public static PrismAppBuilder ConfigureModuleCatalog(this PrismAppBuilder builder, Action<IModuleCatalog> configureCatalog)
         {
-            var services = builder.Builder.Services;
-            services.AddSingleton<IModuleCatalog, ModuleCatalog>();
-            services.AddSingleton<IModuleManager, ModuleManager>();
-            services.AddSingleton<IModuleInitializer, ModuleInitializer>();
-            services.AddSingleton<IMauiInitializeService, PrismModularityInitializationService>();
+            if (!s_didRegisterModules)
+            {
+                var services = builder.Builder.Services;
+                services.AddSingleton<IModuleCatalog, ModuleCatalog>();
+                services.AddSingleton<IModuleManager, ModuleManager>();
+                services.AddSingleton<IModuleInitializer, ModuleInitializer>();
+                services.AddSingleton<IMauiInitializeService, PrismModularityInitializationService>();
+            }
+
+            s_didRegisterModules = true;
             return builder.OnInitialized(container =>
             {
                 var moduleCatalog = container.Resolve<IModuleCatalog>();
