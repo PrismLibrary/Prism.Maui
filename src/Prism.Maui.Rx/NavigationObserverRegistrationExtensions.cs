e2c038bbd1ed6bf3bd7ad1c62d234bc69ae3e4ca
Dan Siegel <me@dansiegel.net>
2022-05-23 13:47:29 -0700

    Add Observables Package

---
+++ src/Prism.Maui.Rx/NavigationObserverRegistrationExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Prism.Ioc;
+
+namespace Prism.Navigation;
+
+public static class NavigationObserverRegistrationExtensions
+{
+    private static bool s_IsRegistered;
+
+    public static IContainerRegistry RegisterGlobalNavigationObserver(this IContainerRegistry container)
+    {
+        if (s_IsRegistered)
+            return container;
+
+        s_IsRegistered = true;
+        return container.RegisterSingleton<IGlobalNavigationObserver, GlobalNavigationObserver>();
+    }
+}
