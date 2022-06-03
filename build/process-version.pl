719b58d6902576353d371aed7554130a73783d6a
Dan Siegel <me@dansiegel.net>
2022-06-03 14:56:42 -0700

    Release pipeline fixes

--- build/process-version.pl
+++ build/process-version.pl
@@ -12,24 +12,29 @@ my $name = $ARGV[0];
 
 my ($gitTag, $version, $preview);
 for my $i (@files) {
-    if($i =~ /$name\.((\d+\.\d+\.\d+)(-[a-zA-Z0-9]+)?)\.nupkg$/) {
+    if($i =~ /$name\.((\d+\.\d+\.\d+)(\-[a-zA-Z0-9\-\.]+)?)\.nupkg$/) {
         $gitTag = $1;
         $version = $2;
         $preview = $3;
+        last;
     }
 }
 
+if(not defined $gitTag or not defined $version) {
+    die "Could not find version information";
+}
+
 if($preview eq "") {
-    print "::set-output name=is-preview::\"false\"\n";
+    print "::set-output name=is-preview::false\n";
     print "::set-output name=release-display-name::$version\n";
 } elsif($preview =~ m/alpha/) {
-    print "::set-output name=is-preview::\"true\"\n";
+    print "::set-output name=is-preview::true\n";
     print "::set-output name=release-display-name::$version - Alpha\n";
 } elsif($preview =~ m/beta/) {
-    print "::set-output name=is-preview::\"true\"\n";
+    print "::set-output name=is-preview::true\n";
     print "::set-output name=release-display-name::$version - Beta\n";
 } else {
-    print "::set-output name=is-preview::\"true\"\n";
+    print "::set-output name=is-preview::true\n";
     print "::set-output name=release-display-name::$version - Preview\n";
 }
 
