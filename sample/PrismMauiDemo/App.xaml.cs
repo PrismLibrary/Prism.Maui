ad1c0c37c55c0c8d41565f4aa60c824e4b5bbdf5
Dan Siegel <me@dansiegel.net>
2022-07-21 08:54:52 -0700

    remove PrismApplication

--- sample/PrismMauiDemo/App.xaml.cs
+++ sample/PrismMauiDemo/App.xaml.cs
@@ -1,23 +1,9 @@
-using Prism;
-using Prism.Navigation;
-
 namespace PrismMauiDemo;
 
-public partial class App : PrismApplication
+public partial class App
 {
     public App()
     {
         InitializeComponent();
-    }
-
-    // Provided mainly for Legacy Support...
-    // This can be done via the builder and a standard Maui Application
-    protected override void OnInitialized()
-    {
-        //var result = await NavigationService.NavigateAsync("MainPage/NavigationPage/ViewA");
-        //if (!result.Success)
-        //{
-        //    System.Diagnostics.Debugger.Break();
-        //}
     }
 }
